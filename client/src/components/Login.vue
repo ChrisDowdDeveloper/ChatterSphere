cd7da3d1a314e167912b46f1cee55d0c83cdfa3a
Chris Dowd <chrisdowd1996@gmail.com>
2024-01-04 13:14:40 -0600

    login page started

---
+++ client/src/components/Login.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+<template>
+    <div class="logo">
+        <img src="" />
+    </div>
+    <div class="form">
+        <h2>Please Log In</h2>
+        <form>
+            <div class="username">
+                <label>Username / Email</label>
+                <input type="text"/>
+            </div>
+            <div class="password">
+                <label>Password</label>
+                <input type="password" />
+            </div>
+        </form>
+    </div>
+</template>
+<style>
+
+</style>
